5fafe82627c4307c6887f1aed5a233ab47b5b1cb
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2023-02-20 11:11:59 +0500

    add image picker on signup

---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({Key? key}) : super(key: key);
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+
+  File? _pickedImageFile;
+  void _pickImage() async{
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    final pickedImageFile = File(pickedImage!.path);
+    // final pickedImageFile = await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _pickedImageFile = pickedImageFile as File;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.grey,
+          backgroundImage: _pickedImageFile != null ?FileImage(_pickedImageFile!) : null,
+        ),
+        TextButton.icon(
+            onPressed: _pickImage,
+            icon: Icon(Icons.image),
+            label: Text("Add Image")),
+      ],
+    );
+  }
+}
